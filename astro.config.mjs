dac96dacba9b74508c17b639160ae1a03aaa6b06
phdRue <galetik@gmail.com>
2024-10-30 22:34:03 +0300

    stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,16 +6,15 @@ import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
 
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
 export default defineConfig({
+  prefetch: true,
   site: "https://example.com",
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind({
-      // Example: Disable injecting a basic `base.css` import on every page.
-      // Useful if you need to define and/or import your own custom `base.css`.
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [mdx(), sitemap(), tailwind({
+    // Example: Disable injecting a basic `base.css` import on every page.
+    // Useful if you need to define and/or import your own custom `base.css`.
+    applyBaseStyles: false,
+  }), alpinejs()],
 });
---
+++ src/utils/navigation.ts
@@ -0,0 +1,11 @@
+// An array of links for navigation bar
+const navBarLinks = [
+    { name: "Home", url: "/" },
+    { name: "Work", url: "/work" },
+    { name: "Pricing", url: "/pricing" },
+    { name: "inspo", url: "/blog" },
+];
+
+export default {
+    navBarLinks,
+};
