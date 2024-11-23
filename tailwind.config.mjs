c180d8f4d46bf53cd35222c669169f66df72535b
phdRue <galetik@gmail.com>
2024-11-23 22:25:49 +0300

    clinic

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,52 @@
-import colors from "@astrojs/tailwind/colors";
+const colors = require('tailwindcss/colors')
+
+//https://coolors.co/2a62ad-0c74bb-286fb7-2673ff-196bb4
+//https://coolors.co/2a62ad-2673ff-286fb7-0c74bb-c93239
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Montserrat']
+      },
       colors: {
         primary: "#1E1A1B",
         primary_bg: "#fffdf1",
+
+        brand: {
+          red: '#C93239',
+          dark_red: '#b4193b',
+
+          actual_red: "#E60045",
+
+          dark_blue: '#1b50b3',
+          // dark_blue: '#0029ad',
+          // dark_blue_1: '#1b50b3',
+
+          // blue: '#286fb7',
+          blue: '#0c74bb',
+          green: '#73ff66',
+          lime: '#a6ff00',
+          cyan: '#66f2ff',
+          // blue: '#2a62ad',
+          
+          // blue: '#196bb4',
+          
+
+          // blue1: "#285EB7",
+          // blue2: "#2a62ad",
+          // blue: "#25408f",
+          
+          gray: colors.neutral[100],
+          white: "#FFFFFF",
+
+          text: {
+            primary: "#000000",
+            secondary: "#B3B3B3"
+          }
+        }
       },
       keyframes: {
         wiggle: {
